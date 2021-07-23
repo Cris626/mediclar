cfe1f8cf2b09484c0ec44cd8fe76883aadaf98d4
Vinci <davinci.dv@gmail.com>
2021-07-23 15:29:55 -0500

    Implementando Vista de Estado, Pantalla Inicial

--- src/views/app/main/form.js
+++ src/views/app/main/form.js
@@ -1,9 +1,120 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowAltCircleLeft } from '@fortawesome/free-solid-svg-icons'
+import { faGlobe } from '@fortawesome/free-solid-svg-icons'
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+// import { faInstagram } from '@fortawesome/free-solid-svg-icons'
+import { faPhoneSquareAlt } from '@fortawesome/free-solid-svg-icons'
+
+import logoMediclarForm from '../../../img/logoForm.jpg'
+import backgroundForm from '../../../img/background-form.jpg'
+import '../../../styles/styleStatus.css';
+
 const Form = () => {
     return(
-        <div>
-            <h1>Esta es la pagina inicial</h1>
+        <div className="container-status-primary">
+
+            <div className="container-status-background">
+                <img src={ backgroundForm } alt="background"/>
+            </div>
+
+            <div className="overlay-status-header"></div>
+
+            <div className="head-icon">
+                <div className="icon-return">
+                    <FontAwesomeIcon className="icon-button" icon={ faArrowAltCircleLeft }/>
+                </div>
+
+                <div className="icon-web">
+                    <FontAwesomeIcon className="icon-style" icon={ faGlobe }/>
+                    <p className="titleIcon">www.mediclarlab.com</p>
+                </div>
+
+                <div className="icon-mail">
+                    <FontAwesomeIcon className="icon-style" icon={ faEnvelope }/>
+                    <p className="titleIcon">info@mediclarlab.com</p>
+                </div>
+
+                <div className="icon-mail">
+                    <FontAwesomeIcon className="icon-style" icon={ ['fab','instagram'] }/>
+                    <p className="titleIcon">mediclar_lab</p>
+                </div>
+
+                <div className="icon-phone">
+                    <FontAwesomeIcon className="icon-style" icon={ faPhoneSquareAlt }/>
+                    <p className="titleIcon">+52 1 33 1441 4922</p>
+                </div>
+
+                <div className="icon-exit">
+                        <FontAwesomeIcon className="icon-button" icon={ faSignOutAlt }/>
+                </div>
+
+            </div>
+
+
+            <div className="container-form-status-primary">
+
+                <div align="center" className="container-logo-primary">
+
+                    <div className="container-logo-register">
+                        <img className="logo-login" src={ logoMediclarForm } alt="logo"/>
+                    </div>
+
+                    <p className="title-logo-login">LISTADO DE PACIENTES</p>
+
+                </div>
+
+                <div className="container-form-status">
+
+                    <form className="form-status">
+
+
+
+
+
+
+                        <div className="container-block-search-primary">
+
+                            <div className="container-block-search">
+
+                                <div className="container-search">
+                                    <label
+                                        htmlFor="status" 
+                                        className="label-form-search"
+                                    >
+                                        Estado
+                                    </label>
+
+                                    <select
+                                        name="status"
+                                        className="input-register-status"
+                                    >
+                                        <option value="">Selection..</option>
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+
+                                    </select>
+                                </div>
+
+                            </div>
+                            
+                        </div>
+
+
+
+
+
+                    </form>
+
+                </div>
+
+
+                <div className="footer">
+                    <p>© 2021 Mediclar es una marca registrada de Barter Box SA de CV</p>
+                </div>
+            </div>
         </div>
     )
 }
