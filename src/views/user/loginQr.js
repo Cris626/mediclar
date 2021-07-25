9bd966b8f9d4106c42102ba8c4a31008a14906f7
Cristian Tito <cristian1997.ctg@gmail.com>
2021-07-25 13:46:57 -0400

    added img qr

--- src/views/user/loginQr.js
+++ src/views/user/loginQr.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../styles/login.css';
 
 import logoMediclar from '../../img/logo.jpg';
@@ -8,11 +8,16 @@ import { loginQR } from '../../redux/actions';
 import { connect } from 'react-redux';
 
 const Login = props => {
+    const [qr, setQr] = useState(props.authUser.dataQr);
 
-    const handleQR=()=>{
-        console.log(props)
-        // props.loginQR(props)
+    const handleQR= async () => {
+        let promise = await Promise.resolve(qr);
+        setQr(promise)
     }
+
+    useEffect(()=>{
+        handleQR();
+    }, [])
 
     return(
         <div className="container-primary">
@@ -24,11 +29,14 @@ const Login = props => {
                 <div align="center" className="container-logo">
                     <img className="logo-login" src={ logoMediclar } alt="logo"/>
                 </div>
-                <div className="container-login">
-                    <p className="session-title">INGRESAR</p>
-                    <button onClick={()=>handleQR()}>
-                        QR
-                    </button>
+                <div className="container-generate-qr">
+                    <div className="container-name-generate-qr">
+                        <p className="session-title"  style={{fontSize: "20px", width: "130px", paddingBottom: "6.5px"}} >INGRESAR</p>
+                        <img className="img-generate-qr" src={`${qr}`} />
+                        <button onClick={()=>props.loginQR(props)}>
+                            QR
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
