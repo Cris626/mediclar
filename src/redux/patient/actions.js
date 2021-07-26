fe15b3d1031d41f5d79cc3b9c16b7bcce3f2f9bf
Cristian Tito <cristian1997.ctg@gmail.com>
2021-07-26 00:35:10 -0400

    added api get location
    ojo con el redux de patient

--- src/redux/patient/actions.js
+++ src/redux/patient/actions.js
@@ -1,12 +1,30 @@
 import axios from 'axios';
 import {
-    REGISTER_PATIENT
+    REGISTER_PATIENT,
+    GET_LOCATION
 } from '../actions';
 
 export const registerPatient = value => {
-    console.log(value)
     return {
         type: REGISTER_PATIENT,
         payload: { ...value }
+    }
+}
+
+/* GET LOCATION */
+
+const getLocationAsync = async () => {
+    let resulLocation = await axios.post('https://sleepy-turing.50-21-189-39.plesk.page/api/v1/locations/get-locations')
+    .then(result => result.data).catch(err => err);
+    return resulLocation;
+}
+
+export const getLocation = () => async dispatch => {
+    let dataLocation = await getLocationAsync();
+    const { data } = dataLocation;
+    const dataResul = Object.values(data);
+    return {
+        type: GET_LOCATION,
+        payload: dataResul
     }
 }
