8486b2a1cd73b6f3103cdc929282d32a84f9b3c4
Cristian Tito <cristian1997.ctg@gmail.com>
2021-07-27 14:27:46 -0400

    added view hotel
    /mediclar/app/result-document/hotel

--- src/views/app/document/index.js
+++ src/views/app/document/index.js
@@ -3,12 +3,19 @@ import { Route, Switch } from 'react-router-dom';
 
 const Result = React.lazy(()=> import('./result'));
 
+const ResultHotel = React.lazy(()=> import('./resultHotel'));
+
 const Document = ({ match }) => (
     <Switch>
         <Route
             exact
             path={`${match.url}`}
             render={props => <Result {...props} />}
+        />
+        <Route
+            exact
+            path={`${match.url}/hotel`}
+            render={props=><ResultHotel {...props} />}
         />
     </Switch>
 )
---
+++ src/views/app/document/resultHotel.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ResultHotel = (props) => {
+    return(
+        <h1>Documento HOTEL</h1>
+    )
+}
+
+export default ResultHotel;
