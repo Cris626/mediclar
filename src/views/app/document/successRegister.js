433b73913c169442fcbbf8081d88a5a33ab5be6c
Cristian Tito <cristian1997.ctg@gmail.com>
2021-07-30 09:40:48 -0400

    added view register successful

---
+++ src/views/app/document/successRegister.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SuccessRegister = props => {
+    return (
+        <div>
+            <h1>Registro exitoso</h1>
+        </div>
+    )
+}
+
+export default SuccessRegister;
